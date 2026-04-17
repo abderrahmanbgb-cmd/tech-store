74db7296ce962c60bda288eecc7fe3a3678cc83f
abderrahmanbgb-cmd <abderrahmanbgb@gmail.com>
2026-04-17 18:14:37 +0100

    main.js

--- css/js/main.js
+++ css/js/main.js
@@ -1,1 +1,27 @@
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+  cart.push({ name, price });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert("Added to cart!");
+}
+
+function displayCart() {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let container = document.getElementById("cart-items");
+  let total = 0;
+
+  container.innerHTML = "";
+
+  cart.forEach(item => {
+    let div = document.createElement("div");
+    div.innerHTML = item.name + " - " + item.price + " DA";
+    container.appendChild(div);
+
+    total += item.price;
+  });
+
+  document.getElementById("total").innerText = "Total: " + total + " DA";
+}
